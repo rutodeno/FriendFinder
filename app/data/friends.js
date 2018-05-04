18e52e6008fe95ed0b8b7e33fb80edeac78c60d5
Dennis Ruto <rutodeno@gmail.com>
2018-05-03 21:02:09 -0400

    edited friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+var friendsArray = [
+
+    {
+        name: "Jerry",
+        photo: "../../../images/JerryMouse.png",
+        score: ["1", "1","1", "1","1", "1","1", "1","1", "1"]
+    },
+
+    {
+        name: "Tom",
+        photo: "../../../images/tomCat.png",
+        score: ["10", "10", "10", "10","10", "10", "10", "10", "10", "10",]
+    }
+]
+
+module.exports = friendsArray;
