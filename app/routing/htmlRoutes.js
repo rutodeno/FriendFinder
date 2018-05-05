a28bc54a9355d3c58d2e811b0a965d3ab09faabe
Dennis Ruto <rutodeno@gmail.com>
2018-05-04 21:48:38 -0400

    edited server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,14 +1,19 @@
 var path = require("path");
 
-module.exports = function(app) {
-    app.get("/", function(req, res){
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-    app.get("/survey", function(req,res){
+    app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-};
 
+        var fileLocation = path.join(__dirname, "../public/survey.html");
+        console.log("HTML File is here: " + fileLocation)
+
+    });
+
+};
 
 
